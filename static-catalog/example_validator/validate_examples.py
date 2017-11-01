6041a5b2e6533737072b2c9da05de74ff968d997
Jeff Naus <jeffnaus@gmail.com>
2017-11-01 14:33:53 -0600

    added examples and json schema

---
+++ static-catalog/example_validator/validate_examples.py
@@ -0,0 +1,19 @@
+import os
+import json
+from jsonschema import validate, RefResolver
+
+# load the json schema for a feature
+with open(os.path.join(os.path.dirname(__file__), '..', 'json-schema', 'feature.json')) as schema_file:
+    schema = json.load(schema_file)
+
+# setup the json schema resolver
+schema_root_url = 'file:///{}/../json-schema/'.format(os.path.dirname(__file__))
+resolver = RefResolver(schema_root_url, 'geojson.json')
+
+# for each example file in the folder validate the schema
+example_directory = os.path.join(os.path.dirname(__file__), '..', 'examples')
+for filename in os.listdir(example_directory):
+    with open(os.path.join(example_directory, filename)) as example_file:
+        example = json.load(example_file)
+    validate(example, schema, resolver=resolver)
+    print('{} validated'.format(filename))
---
+++ static-catalog/readme.md
@@ -0,0 +1,5 @@
+# Static Catalog
+
+A static catalog is an implementation of the STAC specification that is simply a set of files on a web server.  The main purpose of the static catalog is to be crawled by various clients.
+
+* The files of the static catalog will be JSON representations of a [STAC catalog feature](https://github.com/radiantearth/stac-spec/blob/dev/proposed/spec-draft-sprint-day-2.yaml#/definitions/Feature)
