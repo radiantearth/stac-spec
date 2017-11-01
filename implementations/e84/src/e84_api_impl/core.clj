27c3100c53a431f1549dca36c8b0bf7509add442
Chris Holmes <chomie@gmail.com>
2017-11-01 15:12:29 -0700

    removed implementations folder and replaced it with a markdown file with links. The E84 clojure implementation can be found at https://github.com/Element84/catalog-api-spec/tree/dev/implementations/e84

--- implementations/e84/src/e84_api_impl/core.clj
+++
@@ -1,6 +0,0 @@
-(ns e84-api-impl.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- implementations/e84/test/e84_api_impl/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns e84-api-impl.core-test
-  (:require [clojure.test :refer :all]
-            [e84-api-impl.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
