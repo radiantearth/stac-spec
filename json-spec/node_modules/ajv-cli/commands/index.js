a51a432226b716ab1aba44d3aa4553e03b601e42
Chris Holmes <chomie@gmail.com>
2018-03-14 20:35:52 -0700

    got rid of node module stuff

--- json-spec/node_modules/ajv-cli/commands/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    help: require('./help'),
-    validate: require('./validate'),
-    compile: require('./compile'),
-    migrate: require('./migrate'),
-    test: require('./test')
-};
